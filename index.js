63ec898c959f8d7ad0e7c85859f4ce2920034013
manishshahi1 <manish.shahivishen@gmail.com>
2022-09-08 15:04:35 +0530

    123

--- index.js
+++ index.js
@@ -4,24 +4,31 @@ const fs = require("fs");
 // Initialize Express
 const app = express();
 
-let student = { 
-    name: 'Mike',
-    age: 23, 
-    gender: 'Male',
-    department: 'English',
-    car: 'Honda' 
+let student = {
+  name: "Mike",
+  age: 23,
+  gender: "Male",
+  department: "English",
+  car: "Honda",
 };
- 
+
 let data = JSON.stringify(student, null, 2);
 
 // Create GET request
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("Express on Vercel");
+  next();
 });
 
-app.get("/savedata", (req,res) =>{
-    res.send("hello");
-})
+app.get("/savedata", (req, res, next) => {
+  fs.writeFile("student-3.json", data, (err) => {
+    if (err) throw err;
+    console.log("Data written to file");
+  });
+
+  console.log("This is after the write call");
+  next();
+});
 
 // Initialize server
 app.listen(5000, () => {
