c497e485d8fadac359c99b6d226aaac726a73676
manishshahi1 <manish.shahivishen@gmail.com>
2022-09-08 14:57:12 +0530

    added way to save json data

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // Add Express
 const express = require("express");
-
+const fs = require("fs");
 // Initialize Express
 const app = express();
+
+let student = { 
+    name: 'Mike',
+    age: 23, 
+    gender: 'Male',
+    department: 'English',
+    car: 'Honda' 
+};
+ 
+let data = JSON.stringify(student, null, 2);
 
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
+
+app.post("/savedata", (req,res) =>{
+    fs.writeFile('config/data.json', data, (err) => {
+        if (err) throw err;
+        res.send("saved")
+    });
+})
 
 // Initialize server
 app.listen(5000, () => {
